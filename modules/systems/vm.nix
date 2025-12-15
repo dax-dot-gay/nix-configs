54968351a2bafdbd6c6d227537efa85216016c03
Dax Harris <git@dax.gay>
2025-12-15 12:25:10 -0500

    more fix

--- modules/systems/vm.nix
+++ modules/systems/vm.nix
@@ -45,7 +45,6 @@
 
     boot.loader.efi = {
         efiSysMountPoint = "/boot/EFI";
-        canTouchEfiVariables = true;
     };
 
     boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
