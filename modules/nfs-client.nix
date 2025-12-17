1dd2f983a1aded0702009299edc8531c2cee42d1
Dax Harris <git@dax.gay>
2025-12-17 11:52:07 -0500

    use hostname?

--- modules/nfs-client.nix
+++ modules/nfs-client.nix
@@ -1,11 +1,8 @@
 { ... }:
-let
-    hosts = (import ../../lib).hosts;
-in
 {
     boot.supportedFilesystems = [ "nfs" ];
     fileSystems."/shared" = {
-        device = "${(hosts.getHost "infra-nfs").ip}:/shared";
+        device = "infra-nfs.lsb:/shared";
         fsType = "nfs";
         options = [ "nfsvers=4.2" ];
     };
