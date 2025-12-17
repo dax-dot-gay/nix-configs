5d6540acf48f7590f20fd88a742b5d20ad6ce30d
Dax Harris <git@dax.gay>
2025-12-17 12:48:22 -0500

    use /export/shared

--- systems/infra/nfs/configuration.nix
+++ systems/infra/nfs/configuration.nix
@@ -3,7 +3,7 @@
     services.nfs.server = {
         enable = true;
         exports = ''
-            /shared 192.168.30.0/24(no_root_squash,rw,insecure,async,fsid=0,no_subtree_check) 10.1.8.0/24(no_root_squash,rw,insecure,async,fsid=0,no_subtree_check)
+            /export/shared 192.168.30.0/24(no_root_squash,rw,insecure,async,fsid=0,no_subtree_check) 10.1.8.0/24(no_root_squash,rw,insecure,async,fsid=0,no_subtree_check)
         '';
     };
 
