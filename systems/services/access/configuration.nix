ef97807e5d06890e1500e3a5112fc8d170bcf47e
Dax Harris <git@dax.gay>
2025-12-17 19:19:41 -0500

    switch to template

--- systems/services/access/configuration.nix
+++ systems/services/access/configuration.nix
@@ -1,17 +1,25 @@
 { config, ... }:
 {
+    config.sops.templates."services/access/filebrowser.config.json" = {
+        user = "root";
+        content = ''
+            {
+                "root": "/shared",
+                "database": "/shared/systems/services/access/filebrowser.db",
+                "address": "0.0.0.0",
+                "port": 8080,
+                "username": "${config.sops.placeholder."services/access/filebrowser/username"}",
+                "password": "${config.sops.placeholder."services/access/filebrowser/password"}"
+            }
+        '';
+    };
     services.filebrowser = {
         enable = true;
         user = "root";
         group = "root";
         openFirewall = true;
         settings = {
-            root = "/shared";
-            database = "/shared/systems/services/access/filebrowser.db";
-            address = "0.0.0.0";
-            port = 8080;
-            username = config.sops.placeholder."services/access/filebrowser/username";
-            password = config.sops.placeholder."services/access/filebrowser/password";
+            config = config.sops.templates."services/access/filebrowser.config.json".path;
         };
     };
 }
