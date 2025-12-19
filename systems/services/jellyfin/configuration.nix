f633f6c16ca89d4464d5a05b8a67d204dd43e187
Dax Harris <git@dax.gay>
2025-12-19 14:23:16 -0500

    trying to fix nix_ide

--- systems/services/jellyfin/configuration.nix
+++ systems/services/jellyfin/configuration.nix
@@ -1,4 +1,12 @@
-{ ... }:
+{ config, ... }:
 {
-
+    services.jellarr = {
+        enable = true;
+        config = {
+            base_url = "http://0.0.0.0:8096";
+        };
+        bootstrap = {
+            enable = true;
+        };
+    };
 }
