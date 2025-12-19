0b090b08c6b56154e8ae2c9fc36f8e0845a234df
Dax Harris <git@dax.gay>
2025-12-18 20:49:58 -0500

    fix matrix verification cors

--- systems/infra/nginx/preflight.nix
+++ systems/infra/nginx/preflight.nix
@@ -1,7 +1,7 @@
 ''
     if ($request_method = OPTIONS ) {
         add_header 'Access-Control-Allow-Origin'  '*';
-        add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS, HEAD';
+        add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS, HEAD, PUT, DELETE, CONNECT, TRACE, PATCH';
         add_header 'Access-Control-Allow-Headers' 'Authorization, Origin, X-Requested-With, Content-Type, Accept';
 
         return 200;
