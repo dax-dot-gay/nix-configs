bad208e0ebacfd4eeda8796368da198cb90f0e80
Dax Harris <git@dax.gay>
2025-12-19 14:37:20 -0500

    test add nvidia

--- systems/services/jellyfin/configuration.nix
+++ systems/services/jellyfin/configuration.nix
@@ -1,10 +1,18 @@
 { config, ... }:
 {
-    services.jellarr = {
+    /*services.jellarr = {
         enable = true;
-        
+        bootstrap = {
+            enable = true;
+            apiKeyFile = config.sops.secrets."jellyfin/jellarr_key";
+            jellyfinDataDir = "/shared/systems/services/jellyfin";
+        };
         config = {
             base_url = "http://0.0.0.0:8096";
+            
         };
-    };
+    };*/
+
+    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_535;
+    hardware.nvidia.enabled = true;
 }
