9d4fdea1ed3c9a634ec2e7c12e740f6f98066951
Dax Harris <git@dax.gay>
2025-12-20 10:25:20 -0500

    better squashing

--- systems/infra/nfs/configuration.nix
+++ systems/infra/nfs/configuration.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
     services.nfs.server = {
         enable = true;
         exports = ''
-            /export 192.168.30.0/24(no_root_squash,rw,insecure,async,fsid=0,no_subtree_check) 10.1.8.0/24(no_root_squash,rw,insecure,async,fsid=0,no_subtree_check)
-            /export/shared 192.168.30.0/24(no_root_squash,rw,insecure,async,no_subtree_check) 10.1.8.0/24(no_root_squash,rw,insecure,async,no_subtree_check)
+            /export 192.168.30.0/24(all_squash,anonuid=${config.users.users.nfsuser.uid},anongid=${config.users.groups.nfsuser.gid},rw,insecure,async,fsid=0,no_subtree_check) 10.1.8.0/24(all_squash,anonuid=${config.users.users.nfsuser.uid},anongid=${config.users.groups.nfsuser.gid},rw,insecure,async,fsid=0,no_subtree_check)
+            /export/shared 192.168.30.0/24(all_squash,anonuid=${config.users.users.nfsuser.uid},anongid=${config.users.groups.nfsuser.gid},rw,insecure,async,no_subtree_check) 10.1.8.0/24(all_squash,anonuid=${config.users.users.nfsuser.uid},anongid=${config.users.groups.nfsuser.gid},rw,insecure,async,no_subtree_check)
         '';
     };
 
