4df6a1b80cc1bd858070e72781d34c4838526494
Dax Harris <git@dax.gay>
2025-12-20 10:51:09 -0500

    everyone gets root

--- systems/infra/nfs/configuration.nix
+++ systems/infra/nfs/configuration.nix
@@ -1,24 +1,14 @@
 { pkgs, config, ... }:
-let 
-    nfs_uid = builtins.toString config.users.users.nfsuser.uid;
-    nfs_gid = builtins.toString config.users.groups.nfsuser.gid;
-in
 {
     services.nfs.server = {
         enable = true;
         exports = ''
-            /export 192.168.30.0/24(all_squash,anonuid=${nfs_uid},anongid=${nfs_gid},rw,insecure,async,fsid=0,no_subtree_check) 10.1.8.0/24(all_squash,anonuid=0,anongid=0,rw,insecure,async,fsid=0,no_subtree_check)
-            /export/shared 192.168.30.0/24(all_squash,anonuid=${nfs_uid},anongid=${nfs_gid},rw,insecure,async,no_subtree_check) 10.1.8.0/24(all_squash,anonuid=0,anongid=0,rw,insecure,async,no_subtree_check)
+            /export 192.168.30.0/24(all_squash,anonuid=0,anongid=0,rw,insecure,async,fsid=0,no_subtree_check) 10.1.8.0/24(all_squash,anonuid=0,anongid=0,rw,insecure,async,fsid=0,no_subtree_check)
+            /export/shared 192.168.30.0/24(all_squash,anonuid=0,anongid=0,rw,insecure,async,no_subtree_check) 10.1.8.0/24(all_squash,anonuid=0,anongid=0,rw,insecure,async,no_subtree_check)
         '';
     };
 
     networking.firewall.enable = false;
 
     environment.systemPackages = [ pkgs.rclone ];
-    users.users.nfsuser = {
-        group = "nfsuser";
-        isSystemUser = true;
-        uid = 650;
-    };
-    users.groups."nfsuser" = {gid = 650;};
 }
