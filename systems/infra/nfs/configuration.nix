143bb808eda1ac9bb25bd55aca9f2d69d4b74323
Dax Harris <git@dax.gay>
2025-12-20 10:27:21 -0500

    fix gid/uid coerce

--- systems/infra/nfs/configuration.nix
+++ systems/infra/nfs/configuration.nix
@@ -1,10 +1,14 @@
 { pkgs, config, ... }:
+let 
+    nfs_uid = builtins.toString config.users.users.nfsuser.uid;
+    nfs_gid = builtins.toString config.users.groups.nfsuser.gid;
+in
 {
     services.nfs.server = {
         enable = true;
         exports = ''
-            /export 192.168.30.0/24(all_squash,anonuid=${config.users.users.nfsuser.uid},anongid=${config.users.groups.nfsuser.gid},rw,insecure,async,fsid=0,no_subtree_check) 10.1.8.0/24(all_squash,anonuid=${config.users.users.nfsuser.uid},anongid=${config.users.groups.nfsuser.gid},rw,insecure,async,fsid=0,no_subtree_check)
-            /export/shared 192.168.30.0/24(all_squash,anonuid=${config.users.users.nfsuser.uid},anongid=${config.users.groups.nfsuser.gid},rw,insecure,async,no_subtree_check) 10.1.8.0/24(all_squash,anonuid=${config.users.users.nfsuser.uid},anongid=${config.users.groups.nfsuser.gid},rw,insecure,async,no_subtree_check)
+            /export 192.168.30.0/24(all_squash,anonuid=${nfs_uid},anongid=${nfs_gid},rw,insecure,async,fsid=0,no_subtree_check) 10.1.8.0/24(all_squash,anonuid=${nfs_uid},anongid=${nfs_gid},rw,insecure,async,fsid=0,no_subtree_check)
+            /export/shared 192.168.30.0/24(all_squash,anonuid=${nfs_uid},anongid=${nfs_gid},rw,insecure,async,no_subtree_check) 10.1.8.0/24(all_squash,anonuid=${nfs_uid},anongid=${nfs_gid},rw,insecure,async,no_subtree_check)
         '';
     };
 
