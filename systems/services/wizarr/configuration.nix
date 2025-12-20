482a0fbbd56757fcf6d04b7cc40ee5d3c11ed104
Dax Harris <30746447+dax-dot-gay@users.noreply.github.com>
2025-12-20 12:03:37 -0500

    Add wizarr service (#6)
    * add wizarr conf
    
    * try to fix group
    
    * giving the wizard root
    
    * dont set specific user

---
+++ systems/services/wizarr/configuration.nix
@@ -0,0 +1,16 @@
+{ ... }:
+{
+    ensurePaths.folders."/shared/systems/services/wizarr" = { };
+    virtualisation.oci-containers.containers.wizarr = {
+        autoStart = true;
+        image = "ghcr.io/wizarrrr/wizarr";
+        ports = [ "0.0.0.0:5690:5690" ];
+        volumes = [ "/shared/systems/services/wizarr:/data" ];
+        environment = {
+            DISABLE_BUILTIN_AUTH = "false";
+            TZ = "America/New_York";
+        };
+        user = "root:root";
+    };
+    networking.firewall.allowedTCPPorts = [ 5690 ];
+}
