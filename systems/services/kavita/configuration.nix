49b259f7570c2777cfbd91acc83b7edcec13ae70
Dax Harris <git@dax.gay>
2025-12-20 10:43:53 -0500

    nix flake check is lying to me

--- systems/services/kavita/configuration.nix
+++ systems/services/kavita/configuration.nix
@@ -7,7 +7,7 @@
         dataDir = "/shared/systems/services/kavita";
         tokenKeyFile = config.sops.secrets."kavita_key".path;
         port = 5000;
-        ipAdresses = "0.0.0.0";
+        ipAdresses = [ "0.0.0.0" ];
     };
     ensurePaths.folders."/shared/systems/services/kavita" = { };
 }
