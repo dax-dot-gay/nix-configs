37d988622e9d0317c4ac0b7dad35be7edefe7358
Dax Harris <git@dax.gay>
2025-12-21 12:48:45 -0500

    just ip export

--- systems/infra/nfs/configuration.nix
+++ systems/infra/nfs/configuration.nix
@@ -8,8 +8,8 @@ in
     services.nfs.server = {
         enable = true;
         exports = ''
-            /export 192.168.30.0/24(anonuid=${nfsuser},anongid=${nfsgroup},rw,insecure,async,fsid=0,no_subtree_check,root_squash,no_all_squash,crossmnt) ${hosts.ip "services-wizarr"}/24(anonuid=${nfsuser},anongid=${nfsgroup},rw,insecure,async,fsid=0,no_subtree_check,no_root_squash,no_all_squash,crossmnt) 
-            /export/shared 192.168.30.0/24(anonuid=${nfsuser},anongid=${nfsgroup},rw,insecure,async,no_subtree_check,root_squash,no_all_squash,crossmnt) ${hosts.ip "services-wizarr"}/24(anonuid=${nfsuser},anongid=${nfsgroup},rw,insecure,async,fsid=0,no_subtree_check,no_root_squash,no_all_squash,crossmnt)
+            /export 192.168.30.0/24(anonuid=${nfsuser},anongid=${nfsgroup},rw,insecure,async,fsid=0,no_subtree_check,root_squash,no_all_squash,crossmnt) ${hosts.ip "services-wizarr"}(anonuid=${nfsuser},anongid=${nfsgroup},rw,insecure,async,fsid=0,no_subtree_check,no_root_squash,no_all_squash,crossmnt) 
+            /export/shared 192.168.30.0/24(anonuid=${nfsuser},anongid=${nfsgroup},rw,insecure,async,no_subtree_check,root_squash,no_all_squash,crossmnt) ${hosts.ip "services-wizarr"}(anonuid=${nfsuser},anongid=${nfsgroup},rw,insecure,async,fsid=0,no_subtree_check,no_root_squash,no_all_squash,crossmnt)
         '';
     };
 
