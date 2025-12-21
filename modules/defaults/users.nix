bd877ac29fa46059feb1acd8a89463b53a22c5df
Dax Harris <git@dax.gay>
2025-12-21 09:47:24 -0500

    add nfsuser to all systems

--- modules/defaults/users.nix
+++ modules/defaults/users.nix
@@ -6,17 +6,28 @@
             hashedPasswordFile = config.sops.secrets.password.path;
             group = "itec";
             uid = 1000;
-            extraGroups = [ "wheel" ];
+            extraGroups = [ "wheel" "nfsuser" ];
         };
         root = {
             hashedPasswordFile = config.sops.secrets.password.path;
             group = "root";
             uid = 0;
         };
+        nfsuser = {
+            isSystemUser = true;
+            uid = 200;
+            group = "nfsuser";
+        };
     };
+
+    users.mutableUsers = false;
 
     users.groups.itec = {
         name = "itec";
         gid = 1000;
+    };
+    users.groups.nfsuser = {
+        name = "nfsuser";
+        gid = 200;
     };
 }
