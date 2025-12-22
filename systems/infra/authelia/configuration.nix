79ccf132ed951e83ba22987b3a8c1572e175a770
Dax Harris <git@dax.gay>
2025-12-21 19:29:30 -0500

    back to sqlite

--- systems/infra/authelia/configuration.nix
+++ systems/infra/authelia/configuration.nix
@@ -9,7 +9,6 @@
     users.groups.authelia = {};
     ensurePaths.folders."/shared/systems/infra/authelia/assets" = {};
     ensurePaths.files."/shared/systems/infra/authelia/users.yml" = {};
-    ensurePaths.folders."/shared/systems/infra/authelia/postgres" = {};
     services.authelia.instances.lesbosso = {
         enable = true;
         user = "root";
@@ -18,25 +17,4 @@
         secrets.manual = true;
     };
     networking.firewall.enable = false;
-    services.postgresql = {
-        enable = true;
-        authentication = ''
-            #type database  DBuser  auth-method
-            local all       all     trust
-        '';
-        dataDir = "/shared/systems/infra/authelia/postgres";
-        ensureUsers = [
-          {
-            name = "authelia";
-            ensureDBOwnership = true;
-            ensureClauses = {
-                createdb = true;
-                login = true;
-            };
-          }
-        ];
-        ensureDatabases = [
-            "authelia"
-        ];
-    };
 }
