f65524d39f177ae44dd46438274cf5cf158d9ea6
Dax Harris <git@dax.gay>
2026-01-04 13:53:18 -0500

    annoying bash syntax

--- systems/services/access/pre-login-hook.sh
+++ systems/services/access/pre-login-hook.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-
 /run/current-system/sw/bin/rm -rf /shared/systems/services/access/login-attempt.json
 echo "$SFTPGO_LOGIND_USER" > /shared/systems/services/access/login-attempt.json
 USER_ID="$(/run/current-system/sw/bin/jq .id /shared/systems/services/access/login-attempt.json)"
 USER_NAME="$(/run/current-system/sw/bin/jq .username /shared/systems/services/access/login-attempt.json)"
 
-if [$USER_ID -eq 0 && $SFTPGO_LOGIND_PROTOCOL == "OIDC"]; then
+if [ $USER_ID -eq 0 && $SFTPGO_LOGIND_PROTOCOL == "OIDC" ]; then
     echo '{"status": 1, "username": "$USER_NAME"}'
 else
     echo ""
