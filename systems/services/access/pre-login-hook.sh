12a1b07c2f71968f57ea4462fb31462699d4bb3c
Dax Harris <git@dax.gay>
2026-01-04 13:46:29 -0500

    try to read from file

--- systems/services/access/pre-login-hook.sh
+++ systems/services/access/pre-login-hook.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-USER_JSON=$SFTPGO_LOGIND_USER;
-USER_ID="$(/run/current-system/sw/bin/jq .id '${USER_JSON}')"
-USER_NAME="$(/run/current-system/sw/bin/jq .username '${USER_JSON}')"
-
-echo "$SFTPGO_LOGIND_USER" > /shared/attempted_login
+rm -rf /var/lib/sftpgo/login-attempt.json
+echo "$SFTPGO_LOGIND_USER" > /var/lib/sftpgo/login-attempt.json
+USER_ID="$(/run/current-system/sw/bin/jq .id /var/lib/sftpgo/login-attempt.json)"
+USER_NAME="$(/run/current-system/sw/bin/jq .username /var/lib/sftpgo/login-attempt.json)"
 
 if [$USER_ID -eq 0 && $SFTPGO_LOGIND_PROTOCOL == "OIDC"]; then
     echo '{"status": 1, "username": "$USER_NAME"}'
 else
     echo ""
 fi
+
+rm -rf /var/lib/sftpgo/login-attempt.json
