d5aaa233f2bc30bc3240546a027fc24930a1086c
Dax Harris <git@dax.gay>
2026-01-04 14:18:04 -0500

    don't wrap

--- systems/services/access/pre-login-hook.sh
+++ systems/services/access/pre-login-hook.sh
@@ -7,7 +7,7 @@ if [ $USER_ID -eq 0 ]; then
     if [ $SFTPGO_LOGIND_PROTOCOL = "OIDC" ]; then
         JQ_OUT=$(/run/current-system/sw/bin/printf '{"status": 1,"username": "%s","has_password": false,"permissions": {"/": ["*"]}}' "${USER_NAME}")
 
-        echo -e "${JQ_OUT}"
+        echo -e $JQ_OUT
     else
         echo ""
     fi
