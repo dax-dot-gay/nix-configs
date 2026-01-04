e7a3dcf13bb3b65cb61a7c2d8345edf3d0546a0a
Dax Harris <git@dax.gay>
2026-01-04 12:08:00 -0500

    initial sftpgo push

--- systems/services/access/configuration.nix
+++ systems/services/access/configuration.nix
@@ -1,113 +1,74 @@
-{ pkgs, daxlib, hostname, config, ... }:
-let
-    hosts = daxlib.hosts;
-    settings = (pkgs.formats.yaml {}).generate "filebrowser.yml" {
-        server = {
-            disableUpdateCheck = true;
-            disablePreviews = false;
-            listen = "0.0.0.0";
-            port = 8080;
-            database = "/volume/filebrowser.db";
-            sources = [
-                {
-                    path = "/shared";
-                    name = "Filesystem Root";
-                    config = {
-                        denyByDefault = true;
-                        defaultEnabled = false;
-                        createUserDir = false;
-                    };
-                }
-                {
-                    path = "/shared/data";
-                    name = "Data Storage";
-                    config = {
-                        denyByDefault = true;
-                        defaultEnabled = false;
-                        createUserDir = false;
-                    };
-                }
-                {
-                    path = "/shared/data/users";
-                    name = "Personal Files";
-                    config = {
-                        denyByDefault = false;
-                        defaultEnabled = true;
-                        createUserDir = true;
-                    };
-                }
-                {
-                    path = "/shared/data/media";
-                    name = "Media";
-                    config = {
-                        denyByDefault = false;
-                        defaultEnabled = true;
-                        createUserDir = false;
-                    };
-                }
-            ];
-            externalUrl = "https://fs.dax.gay";
-            internalUrl = "http://${hosts.ip hostname}:8080";
-            cacheDir = "/volume/cache";
-            maxArchiveSize = 100;
-        };
-        auth = {
-            tokenExpirationHours = 6;
-            methods.password = {
-                enabled = true;
-                signup = false;
-            };
-            methods.oidc = {
-                enabled = true;
-                issuerUrl = "https://auth.dax.gay/application/o/fs-dax-gay/";
-                scopes = "openid email profile";
-                userIdentifier = "preferred_username";
-                logoutRedirectUrl = "https://auth.dax.gay/application/o/fs-dax-gay/end-session/";
-                createUser = true;
-                adminGroup = "authentik-admin";
-                groupsClaim = "groups";
-            };
-            adminUsername = "admin";
-        };
-        frontend = {
-            name = "Lesbos - File Server";
-            disableDefaultLinks = true;
-            description = "File Storage: Powered by Lesbians!";
-        };
-    };
-in
+{ config, ... }:
 {
     ensurePaths.folders."/shared/systems/services/access" = {};
-    ensurePaths.folders."/shared/systems/services/access/cache" = {};
     ensurePaths.folders."/shared/data/users" = {};
     ensurePaths.folders."/shared/data/public" = {};
     secrets.secrets = {
-        "files/admin_password" = {hosts = ["services-access"];};
-        "files/oidc_client_id" = {hosts = ["services-access"];};
-        "files/oidc_client_secret" = {hosts = ["services-access"];};
-        "files/jwt_secret" = {hosts = ["services-access"];};
-        "files/totp_secret" = {hosts = ["services-access"];};
+        "files/oidc/client_id" = {};
+        "files/oidc/client_secret" = {};
+        "files/oidc/config_url" = {};
+        "files/admin_user" = {};
+        "files/admin_password" = {};
     };
 
-    sops.templates."filebrowser.env".content = ''
-        FILEBROWSER_ADMIN_PASSWORD=${config.sops.placeholder."files/admin_password"}
-        FILEBROWSER_OIDC_CLIENT_ID=${config.sops.placeholder."files/oidc_client_id"}
-        FILEBROWSER_OIDC_CLIENT_SECRET=${config.sops.placeholder."files/oidc_client_secret"}
-        FILEBROWSER_JWT_TOKEN_SECRET=${config.sops.placeholder."files/jwt_secret"}
-        FILEBROWSER_TOTP_SECRET=${config.sops.placeholder."files/totp_secret"}
-        FILEBROWSER_CONFIG=/volume/config.yml
+    sops.templates."sftpgo.env".content = ''
+        SFTPGO_HTTPD__BINDINGS__0__OIDC__CLIENT_ID="${config.sops.placeholder."files/oidc/client_id"}"
+        SFTPGO_HTTPD__BINDINGS__0__OIDC__CLIENT_SECRET="${config.sops.placeholder."files/oidc/client_secret"}"
+        SFTPGO_HTTPD__BINDINGS__0__OIDC__CONFIG_URL="${config.sops.placeholder."files/oidc/config_url"}"
+        SFTPGO_DEFAULT_ADMIN_USERNAME="${config.sops.placeholder."files/admin_user"}"
+        SFTPGO_DEFAULT_ADMIN_PASSWORD="${config.sops.placeholder."files/admin_password"}"
     '';
+
+    systemd.services.sftpgo.serviceConfig = {
+        EnvironmentFile = config.sops.templates."sftpgo.env".path;
+    };
     
-    virtualisation.oci-containers.containers.filebrowser = {
-        autoStart = true;
-        image = "gtstef/filebrowser:beta";
-        volumes = [
-            "/shared/systems/services/access:/volume"
-            "/shared:/shared"
-            "${settings}:/volume/config.yml"
-        ];
-        ports = ["0.0.0.0:8080:8080"];
-        environmentFiles = [ config.sops.templates."filebrowser.env".path ];
-        user = "root:root";
+    services.sftpgo = {
+        enable = true;
+        user = "root";
+        group = "root";
+        dataDir = "/shared/systems/services/access";
+        settings = {
+            sftpd = {
+                bindings = [
+                    {
+                        port = 2022;
+                    }
+                ];
+                password_authentication = false;
+            };
+            httpd = {
+                bindings = [
+                    {
+                        port = 8080;
+                        oidc = {
+                            redirect_base_url = "https://fs.dax.gay";
+                            username_field = "preferred_username";
+                        };
+                        security = {
+                            enabled = true;
+                        };
+                        branding = {
+                            name = "Lesbos File Share";
+                            short_name = "Lesbos FS";
+                        };
+                    }
+                ];
+            };
+            telemetry = {
+                bind_port = 8081;
+                bind_address = "0.0.0.0";
+                enable_profiler = true;
+            };
+            defender = {
+                enabled = true;
+            };
+            data_provider = {
+                driver = "sqlite";
+                name = "/shared/systems/services/access/sftpgo.db";
+                create_default_admin = true;
+
+            };
+        };
     };
 }
