656a354696cbf42eb89933f358c4ba48992fe10d
Dax Harris <git@dax.gay>
2026-01-04 13:21:49 -0500

    debug pre-login

--- systems/services/access/pre-login-hook.sh
+++ systems/services/access/pre-login-hook.sh
@@ -3,6 +3,8 @@
 USER_ID="$(jq .id $SFTPGO_LOGIND_USER)"
 USER_NAME="$(jq .username $SFTPGO_LOGIND_USER)"
 
+echo "$SFTPGO_LOGIND_USER" > /shared/attempted_login
+
 if [[$USER_ID -eq 0 && $SFTPGO_LOGIND_PROTOCOL == "OIDC"]]; then
     echo '{"status": 1, "username": "$USER_NAME"}'
 else
