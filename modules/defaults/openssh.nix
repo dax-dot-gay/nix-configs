9a0ba2cdeee0b773e2b17aadc06b8a2bb92dfc0b
Dax Harris <git@dax.gay>
2026-01-06 15:15:32 -0500

    move keys back

--- modules/defaults/openssh.nix
+++ modules/defaults/openssh.nix
@@ -12,10 +12,8 @@
 
     users.users.itec.openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIyeYCBhJ36mU1+cXEz4wNhbvHZIJRE4MhPtwmDyvDx root@lesbos"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKFsoY66q/ej1AfjYuJ1d2t7RWdKizRi2TCJ73vEP0iq root@lesbos.peer"
     ];
     users.users.root.openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIyeYCBhJ36mU1+cXEz4wNhbvHZIJRE4MhPtwmDyvDx root@lesbos"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKFsoY66q/ej1AfjYuJ1d2t7RWdKizRi2TCJ73vEP0iq root@lesbos.peer"
     ];
 }
