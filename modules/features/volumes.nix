c6925ab0226f9a2885d0a1ade5ed3ecb1d7b94d5
Dax Harris <git@dax.gay>
2026-01-06 19:23:52 -0500

    volume log file

--- modules/features/volumes.nix
+++ modules/features/volumes.nix
@@ -157,7 +157,7 @@ in
                     wants = [ "systemd-tmpfiles-setup.service" ];
                     preStart = "cp /etc/static/rclone-volumes.conf /run/rclone-volumes.conf";
                     script = ''
-                        bash -c "rclone mount ${value.remote.name}:${removeSuffix "/" value.remote.base_path}/${removePrefix "/" value.path} ${name} --daemon --allow-other --vfs-cache-mode writes --cache-dir /var/cache/rclone --config /run/rclone-volumes.conf --uid $(id -u ${value.owner}) --gid $(id -g ${value.group}) --umask ${value.umask} --temp-dir /tmp -vv"
+                        rclone mount ${value.remote.name}:${removeSuffix "/" value.remote.base_path}/${removePrefix "/" value.path} ${name} --daemon --allow-other --vfs-cache-mode writes --cache-dir /var/cache/rclone --config /run/rclone-volumes.conf --uid $(id -u ${value.owner}) --gid $(id -g ${value.group}) --umask ${value.umask} --temp-dir /tmp -vv --log-file /run/rclone.volumes.log
 
                         ${concatStringsSep "\n" (map (subpath: "mkdir -p ${removeSuffix "/" name}/${removePrefix "/" subpath}") value.subpaths)}
                     '';
