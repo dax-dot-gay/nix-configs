4a85526776638c9c621708327b09b07a04ae25a0
Dax Harris <git@dax.gay>
2026-01-13 22:25:10 -0500

    no auto-import

--- systems/services/arr/docker-compose.nix
+++ systems/services/arr/docker-compose.nix
@@ -299,7 +299,7 @@
       lidarr_address = "https://lid.arr.dax.gay";
       thread_limit = "8";
       sync_schedule = "0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23";
-      attempt_lidarr_import = "True";
+      attempt_lidarr_import = "False";
     };
     environmentFiles = [ config.sops.secrets."arr/lidatube.env".path ];
     volumes = [
