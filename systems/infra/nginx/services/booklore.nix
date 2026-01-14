84c284257ac8b9f564b6a15397842a2a3e2d68b3
Dax Harris <git@dax.gay>
2026-01-14 16:06:10 -0500

    increase max body size

--- systems/infra/nginx/services/booklore.nix
+++ systems/infra/nginx/services/booklore.nix
@@ -13,6 +13,7 @@ in
                 proxyWebsockets = true;
                 extraConfig = preflight + ''
                     proxy_set_header X-Forwarded-Port $server_port;
+                    client_max_body_size 10G;
                 '';
             };
         };
