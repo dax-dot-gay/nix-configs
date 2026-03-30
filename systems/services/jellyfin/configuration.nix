264feeb6b40f7c3ec2648be78815a8cf2e2f9115
Dax Harris <git@dax.gay>
2026-03-29 22:02:45 -0400

    switch to local jellyfin store

--- systems/services/jellyfin/configuration.nix
+++ systems/services/jellyfin/configuration.nix
@@ -3,128 +3,25 @@
     ensurePaths.folders."/persistent/jellyfin" = {
         mode = "0777";
     };
+    ensurePaths.folders."/persistent/jellyfin/web" = {
+        mode = "0777";
+    };
+    ensurePaths.folders."/persistent/jellyfin/data" = {
+        mode = "0700";
+    };
     system.activationScripts = {
         jellyfin-web = ''
-            cp -R ${pkgs.jellyfin-web.outPath}/** /persistent/jellyfin/
-            chmod -R 777 /persistent/jellyfin
+            cp -R ${pkgs.jellyfin-web.outPath}/** /persistent/jellyfin/web
+            chmod -R 777 /persistent/jellyfin/web
         '';
-    };
-    sops.templates.jellarr-env = {
-        content = ''
-            JELLARR_API_KEY=${config.sops.placeholder."jellyfin/jellarr_key"}
-        '';
-        owner = config.services.jellarr.user;
-        group = config.services.jellarr.group;
     };
     services.jellyfin = {
         enable = true;
         user = "root";
         group = "root";
-        dataDir = "/shared/systems/services/jellyfin";
+        dataDir = "/persistent/jellyfin/data";
         openFirewall = true;
         package = pkgs.jellyfin;
-    };
-    services.jellarr = {
-        enable = true;
-        user = "root";
-        group = "root";
-        environmentFile = config.sops.templates.jellarr-env.path;
-        bootstrap = {
-            enable = true;
-            apiKeyFile = "${config.sops.secrets."jellyfin/jellarr_key".path}";
-            jellyfinDataDir = "/shared/systems/services/jellyfin";
-        };
-        config = {
-            version = 1;
-            base_url = "http://localhost:8096";
-            system = {
-                enableMetrics = true;
-                pluginRepositories = [
-                    {
-                        name = "Jellyfin Official";
-                        url = "https://repo.jellyfin.org/releases/plugin/manifest.json";
-                        enabled = true;
-                    }
-                    {
-                        name = "Intro Skipper";
-                        url = "https://intro-skipper.org/manifest.json";
-                        enabled = true;
-                    }
-                    {
-                        name = "Jellyfin Enhanced";
-                        url = "https://raw.githubusercontent.com/n00bcodr/jellyfin-plugins/main/10.11/manifest.json";
-                        enabled = true;
-                    }
-                    {
-                        name = "Jellyfin File Transformation";
-                        url = "https://www.iamparadox.dev/jellyfin/plugins/manifest.json";
-                        enabled = true;
-                    }
-                    {
-                        name = "Jellyfin Media Bar";
-                        url = "https://www.iamparadox.dev/jellyfin/plugins/manifest.json";
-                        enabled = true;
-                    }
-                    {
-                        name = "FinTube";
-                        url = "https://raw.githubusercontent.com/AECX/FinTube/master/manifest.json";
-                        enabled = true;
-                    }
-                ];
-                trickplayOptions = {
-                    enableHwAcceleration = true;
-                    enableHwEncoding = true;
-                };
-            };
-            encoding = {
-                enableHardwareEncoding = true;
-                hardwareAccelerationType = "nvenc";
-                hardwareDecodingCodecs = [
-                    "h264"
-                    "hevc"
-                    "mpeg2video"
-                    "vc1"
-                    "vp8"
-                    "vp9"
-                ];
-                enableDecodingColorDepth10Hevc = false;
-                enableDecodingColorDepth10HevcRext = false;
-                enableDecodingColorDepth12HevcRext = false;
-                enableDecodingColorDepth10Vp9 = true;
-                allowHevcEncoding = true;
-                allowAv1Encoding = false;
-            };
-            library = {
-                virtualFolders = [
-                    {
-                        name = "Movies";
-                        collectionType = "movies";
-                        libraryOptions.pathInfos = [ { path = "/shared/data/media/Movies"; } ];
-                    }
-                    {
-                        name = "Shows";
-                        collectionType = "tvshows";
-                        libraryOptions.pathInfos = [ { path = "/shared/data/media/Shows"; } ];
-                    }
-                    {
-                        name = "Songs";
-                        collectionType = "music";
-                        libraryOptions.pathInfos = [ { path = "/shared/data/media/Songs"; } ];
-                    }
-                ];
-            };
-            users = [
-                {
-                    name = "itec";
-                    passwordFile = config.sops.secrets."jellyfin/admin_password".path;
-                    policy = {
-                        isAdministrator = true;
-                        loginAttemptsBeforeLockout = 3;
-                    };
-                }
-            ];
-            startup.completeStartupWizard = true;
-        };
     };
 
     hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_535;
